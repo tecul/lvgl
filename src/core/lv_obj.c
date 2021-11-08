14c411b375ac75321425791473f768f45aeb1acc
ekim <ekim.tecul@yahoo.fr>
2021-11-08 19:37:04 +0100

    Disable LV_OBJ_FLAG_PRESS_LOCK attribute so we can slide off a button without click

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -382,7 +382,8 @@ static void lv_obj_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
     /*Set attributes*/
     obj->flags = LV_OBJ_FLAG_CLICKABLE;
     obj->flags |= LV_OBJ_FLAG_SNAPABLE;
-    if(parent) obj->flags |= LV_OBJ_FLAG_PRESS_LOCK;
+    /* comment this to prevent click event when focus is lost */
+    /*if(parent) obj->flags |= LV_OBJ_FLAG_PRESS_LOCK;*/
     if(parent) obj->flags |= LV_OBJ_FLAG_SCROLL_CHAIN;
     obj->flags |= LV_OBJ_FLAG_CLICK_FOCUSABLE;
     obj->flags |= LV_OBJ_FLAG_SCROLLABLE;
